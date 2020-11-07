1b798ef3d738d369712d6ad841acdd904877de3f
BarWieleba <barwieleba@gmail.com>
2020-11-07 12:15:22 +0100

    initial commit with lab5 changes without the 3rd fragment

---
+++ app/src/main/java/com/example/basiclab/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.basiclab;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.MenuInflater;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu); //OG
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        //int id = item.getItemId();   /OG
+
+        //noinspection SimplifiableIfStatement
+        /*if (id == R.id.action_settings) {
+            return true;                        //OG
+        }*/
+
+        switch (item.getItemId()){
+
+            case R.id.hello_item:
+                hello();
+                return true;
+            case R.id.whoMadeMe:
+                whoMadeMe();
+                return true;
+            case R.id.info:
+                info();
+                return true;
+            case R.id.action_settings:
+                settings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        //return super.onOptionsItemSelected(item); //OG
+    }
+
+    public void hello(){
+        Snackbar.make(getWindow().getDecorView().getRootView(), "Hello there", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+    }
+    public void whoMadeMe(){
+        Snackbar.make(getWindow().getDecorView().getRootView(), "Bartłomiej Wieleba made me", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+    }
+    public void info(){
+        Snackbar.make(getWindow().getDecorView().getRootView(), "I'm just a simple app", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+    }
+    public void settings(){
+        Snackbar.make(getWindow().getDecorView().getRootView(), "This menu item will be changed later", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+    }
+}
